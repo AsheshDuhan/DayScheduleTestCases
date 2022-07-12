706ea6a7895d39149ebbd6a8851829c4092bc5a6
Ashish Duhan <asheshduhan>
2022-07-12 11:20:44 +0530

    updates

--- tests/otoEvent.spec.js
+++ tests/otoEvent.spec.js
@@ -1,45 +1,47 @@
 const {test, expect} = require('@playwright/test');
+
 let WebContext;
 
-test.beforeAll( async({browser}) =>{
-
+test.beforeAll(async({browser})=>{
     const context = await browser.newContext();
-    const page = await context.newPage();
-
+    const page = await context.newPage(); 
     await page.goto('https://app.dayschedule.in/login');
-
     await page.type('#email', 'ashish.duhan@agenty.com');
     await page.fill('#password', 'Ashi$h2710');
-    await page.click('[type="submit"]');
+    await page.click("[type='submit']");
 
-    await page.waitForLoadState("networkidle");
-
-    await page.click('.close');
-
-    await context.storageState({path:'state.json'});  //when new context created data stored in json path 
-
-    webContext = await browser.newContext({storageSate:'state.json'}); // let webcontext as global variable
+    await page.waitForLoadState('networkidle');
+    const closebtn = page.locator('.close');
+    await closebtn.click();
+    await context.storageState({path: 'state.json'});
+    WebContext = await browser.newContext({storageState:'state.json'});
 
 });
 
-
-    test('clicking on event button', async()=>{
-        const page = await WebContext.newPage();
-        await page.goto('https://app.dayschedule.in/resources');
-        
-        const addbtn = page.locator('.col-6 button');
-        const eventbtn = await page.locator('.dropdown-menu.show a').nth(0);
+test('clicking on new event', async()=>{
+    const page = await WebContext.newPage();
+    await page.goto('https://app.dayschedule.in/resources');
     
-        await addbtn.click();
-        await eventbtn.click();
-    });
-    // const page = await webContext.newPage();
-    // await page.goto('https://app.dayschedule.in/resources');
+    const addbtn = page.locator('.col-6 button');
+    const newEvent = await page.locator('.dropdown-menu.show a[href="/resources/services/add"]');
 
-    // const addbtn = page.locator('.col-6 button');
-    // await addbtn.click();
+    await addbtn.click();
+    await newEvent.click();
+});
 
+test('create services', async()=>{
+    const page = await WebContext.newPage();
+    await page.goto('https://app.dayschedule.in/resources/services/add');
 
-    // await page.pause();
+    const businessname = page.locator('input.ng-pristine').nth(0);
+    await businessname.type('Hair Style');
 
+    // await page.mouse.down();
 
+    // const location = page.locator('.ng-input').last();
+    // const mylocation = page.locator('.media-body').first();
+    // // const newlocation = page.locator('.close');
+    // const newlocation = page.locator('[name=close]');
+    // const newlocation1 = page.locator('.right');
+
+});
