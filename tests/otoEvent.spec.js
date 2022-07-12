6e3dd76b9e5925ba19a6208de04b99731aa8c142
Ashish Duhan <asheshduhan>
2022-07-12 11:56:29 +0530

    insert

--- tests/otoEvent.spec.js
+++ tests/otoEvent.spec.js
@@ -23,25 +23,22 @@ test('clicking on new event', async()=>{
     await page.goto('https://app.dayschedule.in/resources');
     
     const addbtn = page.locator('.col-6 button');
-    const newEvent = await page.locator('.dropdown-menu.show a[href="/resources/services/add"]');
-
     await addbtn.click();
-    await newEvent.click();
+   
+    const btn = "New Event";
+    const eventbtn = page.locator('a.dropdown-item');
+    const count = await eventbtn.count();
+    for(let i=0; i<count; ++i){
+
+        if (await eventbtn.nth(i).locator("i").textContent()==btn)
+        {
+            await eventbtn.nth(i).locator("text = New Event").click();
+            break;
+        }
+
+    }
+    await page.goto('https://app.dayschedule.in/resources/events/add');
+    await page.title();
+    console.log(await expect(page).toHaveTitle('Events - DaySchedule'));
 });
 
-test('create services', async()=>{
-    const page = await WebContext.newPage();
-    await page.goto('https://app.dayschedule.in/resources/services/add');
-
-    const businessname = page.locator('input.ng-pristine').nth(0);
-    await businessname.type('Hair Style');
-
-    // await page.mouse.down();
-
-    // const location = page.locator('.ng-input').last();
-    // const mylocation = page.locator('.media-body').first();
-    // // const newlocation = page.locator('.close');
-    // const newlocation = page.locator('[name=close]');
-    // const newlocation1 = page.locator('.right');
-
-});
